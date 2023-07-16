d645f4647104cc7baf45ea97ecbb0c22ad9ba818
Christian <christiandoramo@gmail.com>
2023-07-16 12:48:10 -0300

    questao 1,3,4 feita

--- lista 3 AOC/Questao 02.asm
+++ lista 3 AOC/Questao 02.asm
@@ -14,3 +14,78 @@
 # Recebe outro char (C2) do usuário;
 # Substitui todas as ocorrências do char C1 na string S pelo char C2;
 # Imprime a nova string com os caracteres substituídos
+
+.data
+    promptS:    .asciiz "Digite uma string: "
+    promptC1:   .asciiz "Digite o caractere a ser substituído: "
+    promptC2:   .asciiz "Digite o caractere substituto: "
+    result:     .asciiz "Nova string com os caracteres substituídos: "
+
+.text
+.globl main
+main:
+    # Prompt para a string S
+    li $v0, 4
+    la $a0, promptS
+    syscall
+
+    # Ler a string S
+    li $v0, 8
+    la $a0, s
+    li $a1, 256  # Tamanho máximo da string
+    syscall
+
+    # Prompt para o caractere C1
+    li $v0, 4
+    la $a0, promptC1
+    syscall
+
+    # Ler o caractere C1
+    li $v0, 12
+    syscall
+    move $t0, $v0  # Salvar o caractere C1 em $t0
+
+    # Prompt para o caractere C2
+    li $v0, 4
+    la $a0, promptC2
+    syscall
+
+    # Ler o caractere C2
+    li $v0, 12
+    syscall
+    move $t1, $v0  # Salvar o caractere C2 em $t1
+
+    # Substituir os caracteres
+    li $v0, 4
+    la $a0, result
+    syscall
+
+    li $t2, 0  # Índice para percorrer a string S
+loop:
+    lb $t3, s($t2)  # Carregar um caractere da string S
+
+    beqz $t3, end  # Se chegamos ao final da string, sair do loop
+
+    beq $t3, $t0, replace  # Se o caractere é igual a C1, substituir
+
+    li $v0, 11  # Imprimir o caractere original
+    move $a0, $t3
+    syscall
+
+    addi $t2, $t2, 1  # Avançar para o próximo caractere
+    j loop
+
+replace:
+    li $v0, 11  # Imprimir o caractere substituto
+    move $a0, $t1
+    syscall
+
+    addi $t2, $t2, 1  # Avançar para o próximo caractere
+    j loop
+
+end:
+    li $v0, 10  # Sair do programa
+    syscall
+
+.data
+s:  .space 256  # Espaço para a string S
--- lista 3 AOC/Questao 03.asm
+++ lista 3 AOC/Questao 03.asm
@@ -15,3 +15,84 @@
 # 1, 2, 3, 4
 # …
 # 1, 2, 3, 4, 5, …, N-1
+
+.data
+msgInicial: .asciiz "Digite um número inteiro maior que 0 N: "
+msgReset: .asciiz "Digite um inteiro maior ou igual a 1"
+sequencia: .asciiz "Sequência: "
+virgula: .asciiz ", "
+linha: .asciiz "\n"
+
+
+.text
+main:
+    li $v0, 4               # print string
+    la $a0, msgInicial     # msgInicial para o usuário digitar N
+    syscall
+
+    li $v0, 5 # leia o inteiro N
+    syscall
+    addi $t0, $v0, 0    # t0 = N
+
+    ble $t0, $0, resetar# checando se é inteiro é menor ou igual a 0
+
+    la $a0, sequencia     # print nome sequência
+    li $v0, 4             # print string
+    syscall
+
+    addi $t1, $0 , 0    # t1 recebe 1 (t1 é o limitador para N linhas)
+loopDaLinha:
+    addi $t2, $0, 2      # t2 recebe 2 (t2 é o limitador para o ultimo numero de uma linha)
+loopDalinhaImprimeLoop:
+    addi $t1, $t1 , 1    # t1 recebe t1 + 1 sempre que entrar aqui, pois é uma nova linha - novo limite
+
+    jal pulaLinha
+
+    li $v0, 1            # imprime inteiro
+    addi $a0, $0, 1     # Imprime o número primeiro de toda linha
+    syscall
+
+    addi $t2, $0, 2      # t2 recebe 2 (t2 é o limitador para o ultimo numero de uma linha)
+imprimeLoop:
+    ble $t2, $t1, imprimeVirgula    # se t2 < t1 então tem um prox numero a linha (logo precisa de ,)
+    addi $t2, $t2, 1        # t2 = 2 + 1 -> t2 = 3 + 1  -> t2 = 4 + 1... t1 = N
+    bgt $t2, $t1, pularLinha
+    j Fim
+    pularLinha:
+         j loopDalinhaImprimeLoop # faz com que t2 continue sedo t2 e imprime 1 e 2
+    
+
+   
+
+imprimeVirgula:
+    li $v0, 4       # imprime string
+    la $a0, virgula # imprime virgula
+    syscall
+
+    addi $a0, $t2, 0  # vai imprimir t2
+    li $v0, 1         # Imprime t2
+    syscall
+
+    addi $t2, $t2, 1 # t2 ++ será 2 novamente no loop da linha 1 ate N-1
+
+    beq $t2, $t0, Fim           # ja pode finalizar pois t1 = t2 = N impressao final
+
+    beq $t2, $t1, loopDaLinha   # se t1 <= N continue prox linha caso p contrario  o programa encerra
+ 	
+ 	# CASO addi $t2, $t2, 1 # t2 ++ será 2 novamente no loop da linha 1 ate N	
+    j imprimeLoop
+
+pulaLinha:
+    li $v0, 4
+    la $a0, linha
+    syscall
+    jr $ra
+
+resetar:
+    li $v0, 4               # print string
+    la $a0, msgReset     #  para o usuário digitar N >=1
+    syscall
+    j main
+Fim:
+    li $v0, 10
+    syscall
