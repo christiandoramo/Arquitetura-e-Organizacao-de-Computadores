f766aa38de8710d63eef4cf623d31a3e8a3ffe68
Christian <christiandoramo@gmail.com>
2023-07-16 17:23:33 -0300

    questao feitas 1,2,3,4

--- lista 3 AOC/Questao 02.asm
+++ lista 3 AOC/Questao 02.asm
@@ -6,86 +6,85 @@
 # semestre letivo: 2022.2
 
 # Questão: 2
- 
+
 
 # Escreva um programa que executa os seguintes passos:
-# Recebe uma string (S) do usuário;
-# Recebe um char (C1) do usuário;
-# Recebe outro char (C2) do usuário;
-# Substitui todas as ocorrências do char C1 na string S pelo char C2;
+# Recebe,     uma,    string,         (S),do,usuário
+# Recebe,     um,     char,           (C1),do,usuário
+# Recebe,     outro,  char,           (C2),do,usuário,
+# Substitui,  todas,  as,             ocorrências,do,char,C1,na,string,S,pelo,char,C2
+# Substitui  todas,      as,     ocorrências,    do,char,C1,na,string,S,pelo,char,C2
 # Imprime a nova string com os caracteres substituídos
 
 .data
-    promptS:    .asciiz "Digite uma string: "
-    promptC1:   .asciiz "Digite o caractere a ser substituído: "
-    promptC2:   .asciiz "Digite o caractere substituto: "
-    result:     .asciiz "Nova string com os caracteres substituídos: "
-
+msgS:    .asciiz "Digite uma string: "
+msgC1:   .asciiz "Digite o caractere a ser substituído: "
+msgC2:   .asciiz "Digite o caractere substituto: "
+saida:     .asciiz "Nova string com os caracteres substituídos: "
+C1:         .space 1                                                # Espaço para o caractere C1
+C2:         .space 1                                                # Espaço para o caractere C2
+S:          .space 1024                                             # Espaço para a string S
+rodou: .asciiz "rodou o loop mais uma vez"
 .text
 .globl main
 main:
-    # Prompt para a string S
-    li $v0, 4
-    la $a0, promptS
-    syscall
 
-    # Ler a string S
-    li $v0, 8
-    la $a0, s
-    li $a1, 256  # Tamanho máximo da string
-    syscall
+# S
+    li      $v0,        4                                           # imprima string
+    la      $a0,        msgS                                        # leia msgS
+    syscall 
+    li      $v0,        8                                           # leia String a partir da syscall
+    la      $a0,        S                                           # traga endereço de S para o a0
+    li      $a1,        1024                                        # Tamanho máximo - certos bytes
+    syscall 
 
-    # Prompt para o caractere C1
-    li $v0, 4
-    la $a0, promptC1
-    syscall
+# C1
+    li      $v0,        4                                           # imprima string
+    la      $a0,        msgC1                                       # carregue endereço da string a imprimir C1
+    syscall 
+    li      $v0,        12                                          # leia caractere
+    syscall 
+    sb      $v0,        C1                                          # guarde o byte/caractere de v0 em C1
 
-    # Ler o caractere C1
-    li $v0, 12
-    syscall
-    move $t0, $v0  # Salvar o caractere C1 em $t0
+# C2
+    li      $v0,        4                                           # imprima string
+    la      $a0,        msgC2                                       # leia endereço da string a imprimir C1
+    syscall 
+    li      $v0,        12                                          # leia caractere
+    syscall 
+    sb      $v0,        C2                                          # Armazenar vyte/caractere de v0 em C2
 
-    # Prompt para o caractere C2
-    li $v0, 4
-    la $a0, promptC2
-    syscall
 
-    # Ler o caractere C2
-    li $v0, 12
-    syscall
-    move $t1, $v0  # Salvar o caractere C2 em $t1
 
-    # Substituir os caracteres
-    li $v0, 4
-    la $a0, result
-    syscall
+# Substituindo -------------------------------------------------------------------------------------------------
+    la      $a0,        S
+    jal     trocaChars
 
-    li $t2, 0  # Índice para percorrer a string S
+# Encerra o programa
+    li      $v0,        10
+    syscall 
+
+trocaChars:                                                         # Carregar o endereço base da string S3
+    lb      $t0,        C1                                          # Carregar o caractere C1
+    lb      $t1,        C2                                          # Carregar o caractere C2
+    addi    $t4,        $a0,    0                                   # Salva o endereço base da string S em $t4
+    addiu   $t2,        $t4,    0,      #,Índice,para,percorrer,a,string,S# Índice para percorrer a string S
 loop:
-    lb $t3, s($t2)  # Carregar um caractere da string S
-
-    beqz $t3, end  # Se chegamos ao final da string, sair do loop
-
-    beq $t3, $t0, replace  # Se o caractere é igual a C1, substituir
-
-    li $v0, 11  # Imprimir o caractere original
-    move $a0, $t3
-    syscall
-
-    addi $t2, $t2, 1  # Avançar para o próximo caractere
-    j loop
-
-replace:
-    li $v0, 11  # Imprimir o caractere substituto
-    move $a0, $t1
-    syscall
-
-    addi $t2, $t2, 1  # Avançar para o próximo caractere
-    j loop
+    lbu     $t3,        0($t2)                                      # ERROR - Carregar byte/char na posição atual de a0 dentro de S para t3
+    beq     $t3,        $0,     end                                 # Se acabou a string, sair do loop
+    beq     $t3,        $t0,    troca                               # Se o caractere é igual a C1, substituir por C2
+    addiu   $t2,        $t2,    1                                   # Avançar para o próximo byte dentro S
+    j       loop                                                    # i.e. a cada loop => a0 + 1 byte = 1($s1), a0 +2 = 2($s1)
+troca:
+    sb      $t1,        0($t2)                                      # troca C1 por C2
+    addiu   $t2,        $t2,    1                                   # Avançar para o próximo byte dentro de S
+    j       loop
 
 end:
-    li $v0, 10  # Sair do programa
-    syscall
+    li      $v0,        4
+    la      $a0,        saida
+    syscall 
+    li      $v0,        4
+    la      $a0,        S
+    syscall 
 
-.data
-s:  .space 256  # Espaço para a string S
--- lista 3 AOC/Questao 04.asm
+++ lista 3 AOC/Questao 04.asm
@@ -6,115 +6,115 @@
 # semestre letivo: 2022.2
 
 # Questão: 4
- 
+
 # Escreva um programa que lê do usuário um inteiro N de 32 bits que corresponde a um valor de tempo em segundos e
-# escreve na tela o tempo equivalente no seguinte formato: 
+# escreve na tela o tempo equivalente no seguinte formato:
 # “Y anos, M meses, D dias, H horas, M minutos e S segundos”
 
-# OBS: por simplicidade, considere que o ano é composto de 12 meses de 30 dias. 
+# OBS: por simplicidade, considere que o ano é composto de 12 meses de 30 dias.
 # OBS2: Seu programa deve ser capaz de receber um valor de até 4.294.967.295 segundos e calcular o tempo corretamente.
-# OBS2: A informação final deve ser de tal forma que represente o tempo total fornecido pelo usuário. 
-# Por exemplo. 122 segundos deve fornecer uma mensagem de 
-# “0 anos, 0 meses, 0 dias, 0 horas, 2 minutos e 2 segundos”. 
-# Por exemplo (2), 90000 segundos deve fornecer uma mensagem de 
+# OBS2: A informação final deve ser de tal forma que represente o tempo total fornecido pelo usuário.
+# Por exemplo. 122 segundos deve fornecer uma mensagem de
+# “0 anos, 0 meses, 0 dias, 0 horas, 2 minutos e 2 segundos”.
+# Por exemplo (2), 90000 segundos deve fornecer uma mensagem de
 # “0 anos, 0 meses, 1 dias, 1 horas, 0 minutos e 0 segundos”.
 
 .data
-    msgInicial:     .asciiz "Digite um valor de tempo em segundos: "
-    msgFinal:     .asciiz "O tempo equivalente é: "
-    anos:      .asciiz " anos, "
-    meses:     .asciiz " meses, "
-    dias:       .asciiz " dias, "
-    horas:      .asciiz " horas, "
-    min:    .asciiz " minutos e "
-    segundos:    .asciiz " segundos\n"
+msgInicial:     .asciiz "Digite um valor de tempo em segundos: "
+msgFinal:     .asciiz "O tempo equivalente é: "
+anos:      .asciiz " anos, "
+meses:     .asciiz " meses, "
+dias:       .asciiz " dias, "
+horas:      .asciiz " horas, "
+min:    .asciiz " minutos e "
+segundos:    .asciiz " segundos\n"
 
 .text
-    li $v0, 4           # imprimindo msgInicia
-    la $a0, msgInicial  # lendo msg
-    syscall
+    li      $v0,    4                                           # imprimindo msgInicia
+    la      $a0,    msgInicial                                  # lendo msg
+    syscall 
 
-    li $v0, 5 # ler inteiro
-    syscall
-    addiu $t0, $v0, 0  #t0 = entrada em v0
+    li      $v0,    5                                           # ler inteiro
+    syscall 
+    addiu   $t0,    $v0,        0                               # t0 = entrada em v0
 
-    li $t1, 31536000   # t1 = 1 ano em segundos (60 * 60 * 24 * 30 365)
-    divu $t0, $t1      # N dividido por segundos em ANOS
-    mflo $s0           # Quociente de N/anos para s0 é anos
-    mfhi $t1           # Resto da divisão N/anos para t1 
+    li      $t1,    31536000                                    # t1 = 1 ano em segundos (60 * 60 * 24 * 30 365)
+    divu    $t0,    $t1                                         # N dividido por segundos em ANOS
+    mflo    $s0                                                 # Quociente de N/anos para s0 é anos
+    mfhi    $t1                                                 # Resto da divisão N/anos para t1
 
-    li $t0, 2592000    # t0 = 1 mês em segundos (60 * 60 * 24 * 30)
-    divu $t1, $t0      # Resto da divisão N/anos por meses em segundos
-    mflo $s1           # Quociente de RestoDaDivisaoAnos/mesesEmSegundos para s1 é meses
-    mfhi $t2           # RestodadivisãoAnos/mes para t2 
+    li      $t0,    2592000                                     # t0 = 1 mês em segundos (60 * 60 * 24 * 30)
+    divu    $t1,    $t0                                         # Resto da divisão N/anos por meses em segundos
+    mflo    $s1                                                 # Quociente de RestoDaDivisaoAnos/mesesEmSegundos para s1 é meses
+    mfhi    $t2                                                 # RestodadivisãoAnos/mes para t2
 
-    li $t0, 86400      # t0 = 1 dia em segundos (60  * 60  * 24)
-    divu $t2, $t0      # resto em dias / dias em segundos
-    mflo $s2           # Quociente de RestoDaDivisaoMeses/DiasEmSegundos para s2 é dias
-    mfhi $t3           # Resto da divisão Meses/dias para t3 
+    li      $t0,    86400                                       # t0 = 1 dia em segundos (60  * 60  * 24)
+    divu    $t2,    $t0                                         # resto em dias / dias em segundos
+    mflo    $s2                                                 # Quociente de RestoDaDivisaoMeses/DiasEmSegundos para s2 é dias
+    mfhi    $t3                                                 # Resto da divisão Meses/dias para t3
 
-    li $t0, 3600       # t0 = 1 hora em segundos (60 * 60)
-    divu $t3, $t0      # resto em minutos / minutos em segundos
-    mflo $s3           # Quociente de RestoDaDivisaoDias/HorasEMSegundos para s3 é horas
-    mfhi $t4           # resta da divisão dias/horas para t4
+    li      $t0,    3600                                        # t0 = 1 hora em segundos (60 * 60)
+    divu    $t3,    $t0                                         # resto em minutos / minutos em segundos
+    mflo    $s3                                                 # Quociente de RestoDaDivisaoDias/HorasEMSegundos para s3 é horas
+    mfhi    $t4                                                 # resta da divisão dias/horas para t4
 
-    li $t0, 60         # t0 = 1 min (60 segundos)
-    divu $t4, $t0      # resto em horas / minutos em segundos
-    mflo $s4           # Quociente de RestoDaDivisaoHoras/MinutosemSegundos para s4 é minutos
-    mfhi $s5           # resta da divisão horas/minutos para t5 -> t5 é segundos
+    li      $t0,    60                                          # t0 = 1 min (60 segundos)
+    divu    $t4,    $t0                                         # resto em horas / minutos em segundos
+    mflo    $s4                                                 # Quociente de RestoDaDivisaoHoras/MinutosemSegundos para s4 é minutos
+    mfhi    $s5                                                 # resta da divisão horas/minutos para t5 -> t5 é segundos
 
-    # printando msg final
-    li $v0, 4
-    la $a0, msgFinal
-    syscall
+# printando msg final
+    li      $v0,    4
+    la      $a0,    msgFinal
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s0, 0      # anos
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s0,        0                               # anos
+    syscall 
 
-    li $v0, 4
-    la $a0, anos
-    syscall
+    li      $v0,    4
+    la      $a0,    anos
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s1, 0      # meses
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s1,        0                               # meses
+    syscall 
 
-    li $v0, 4
-    la $a0, meses
-    syscall
+    li      $v0,    4
+    la      $a0,    meses
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s2, 0      # dias
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s2,        0                               # dias
+    syscall 
 
-    li $v0, 4
-    la $a0, dias
-    syscall
+    li      $v0,    4
+    la      $a0,    dias
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s3, 0     # horas
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s3,        0                               # horas
+    syscall 
 
-    li $v0, 4
-    la $a0, horas
-    syscall
+    li      $v0,    4
+    la      $a0,    horas
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s4, 0      # min
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s4,        0                               # min
+    syscall 
 
-    li $v0, 4
-    la $a0, min
-    syscall
+    li      $v0,    4
+    la      $a0,    min
+    syscall 
 
-    li $v0, 1
-    addiu $a0, $s5, 0      # segundos
-    syscall
+    li      $v0,    1
+    addiu   $a0,    $s5,        0                               # segundos
+    syscall 
 
-    li $v0, 4
-    la $a0, segundos
-    syscall
+    li      $v0,    4
+    la      $a0,    segundos
+    syscall 
 
-    li $v0, 10    # fim do programa
-    syscall
+    li      $v0,    10                                          # fim do programa
+    syscall 
